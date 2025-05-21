0a02794ed6df50a69ffcebfd87ed1bba6417e080
Maya4ok <maya4ok-dev@ya.ru>
2025-05-21 13:40:38 +0500

    Improve logger: timestamps, file output, colors, and optimizations

--- logger.hpp
+++ logger.hpp
@@ -1,8 +1,45 @@
 #pragma once
 
+#if __cplusplus < 201703L
+#error "For now, this library requires C++17 or higher."
+#endif
+
+
+#ifdef _WIN32
+
+#include <windows.h>
+/**
+ * @brief Enables virtual terminal processing in the console.
+ * @return true if successful, false if not.
+ * @details
+ * This function enables virtual terminal processing in the console. This is
+ * required to use ANSI escape codes on Windows. If the function fails, it
+ * returns false. Otherwise, it returns true.
+ * @note This function is only available on Windows.
+ */
+bool enableVirtualTerminal() {
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hOut == INVALID_HANDLE_VALUE) return false;
+
+    DWORD dwMode = 0;
+    if (!GetConsoleMode(hOut, &dwMode)) return false;
+
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    if (!SetConsoleMode(hOut, dwMode)) return false;
+
+    return true;
+}
+#endif
+
+
 #include <iostream>
 #include <string_view>
+#include <fstream>
 #include <mutex>
+#include <chrono>
+#include <ctime>
+#include <sstream>
+#include <iomanip>
 
 // A simple logging system from MayakUI.
 // Made with love by Maya4ok! ❤️
@@ -11,20 +48,89 @@
 // Is thread-safe!
 namespace mayak::logger {
 
+    // Time formats
+    // For getTimestamp() method
+    enum class TimeFormat {
+        DateOnly, // YYYY-MM-DD
+        TimeOnly, // HH:MM:SS
+        DateTime, // YYYY-MM-DD HH:MM:SS
+        FileCompatible // YYYY-MM-DD_HH-MM-SS
+    };
+
     // Log levels.
     // If you set the log level to INFO, only INFO and higher will be logged.
-    enum LogLevel {
+    enum class LogLevel : uint8_t {
         TRACE,
         DEBUG,
         INFO,
         WARN,
-        ERROR,
+        ERR,
         FATAL
     };
 
+        /**
+     * @brief Returns the current timestamp as a string.
+     * @param format The format of the timestamp. Default is TimeFormat::DateTime.
+     * @return The current timestamp as a string.
+     * @details
+     * The returned string will be in the format specified by the TimeFormat parameter.
+     * The default format is YYYY-MM-DD HH:MM:SS.
+     * TimeFormat::DateOnly returns YYYY-MM-DD.
+     * TimeFormat::TimeOnly returns HH:MM:SS.
+     * TimeFormat::FileCompatible returns YYYY-MM-DD_HH-MM-SS.
+     * @note Not only internal, but also used by MAYAK_LOG_TRACE(), MAYAK_LOG_DEBUG(), etc.
+     */
+    inline std::string getTimestamp(TimeFormat format = TimeFormat::DateTime) {
+        auto now = std::chrono::system_clock::now();
+        std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+
+        std::tm now_tm{};
+        #ifdef _WIN32
+            localtime_s(&now_tm, &now_c);
+        #else
+            localtime_r(&now_c, &now_tm);
+        #endif
+
+        std::stringstream ss;
+        switch (format) {
+            case TimeFormat::DateOnly:
+                ss << std::put_time(&now_tm, "%Y-%m-%d");
+                break;
+            case TimeFormat::TimeOnly:
+                ss << std::put_time(&now_tm, "%H:%M:%S");
+                break;
+            case TimeFormat::DateTime:
+                ss << std::put_time(&now_tm, "%Y-%m-%d %H:%M:%S");
+                break;
+            case TimeFormat::FileCompatible:
+                ss << std::put_time(&now_tm, "%Y-%m-%d_%H-%M-%S");
+                break;
+        }
+        return ss.str();
+    }
+
     inline bool additionalInfo = false;
-    inline LogLevel logLevel = INFO;
+    inline LogLevel logLevel = LogLevel::INFO;
     inline std::mutex logMutex;
+    inline std::string logTimestamp = getTimestamp(TimeFormat::FileCompatible);
+    inline std::string logFilename = "mayak_log_" + logTimestamp + ".log";
+    inline bool fileLogging = true;
+    inline bool consoleLogging = true;
+    #ifdef _WIN32
+    inline bool colorLogging = enableVirtualTerminal();
+    #else
+    inline bool colorLogging = true;
+    #endif
+
+    /**
+     * Sets the filename of the log file.
+     * The filename will be changed to <filename>_<timestamp>.<extension>.
+     * @param filename The new filename.
+     * @param extension The new file extension.
+     */
+    inline void setFilename(std::string_view filename, std::string_view extension) {
+        logFilename = std::string(filename) + "_" + getTimestamp(TimeFormat::FileCompatible) + "." + std::string(extension);
+    }
 
     /**
      * Sets the current log level.
@@ -45,16 +151,52 @@ namespace mayak::logger {
         additionalInfo = value;
     }
 
+    /**
+     * Sets whether the logger will log to a file.
+     * @param value whether to log to a file.
+     */
+    void setFileLogging(bool value) {
+        fileLogging = value;
+    }
+
+    /**
+     * Sets whether the logger will log to the console.
+     * @param value whether to log to the console.
+     */
+    void setConsoleLogging(bool value) {
+        consoleLogging = value;
+    }
+
+    /**
+     * Sets whether the logger will use colors.
+     * @param value whether to use colors.
+     */
+    void setColorLogging(bool value) {
+        colorLogging = value;
+    }
+
+    inline std::ofstream& getLogFile() {
+        static std::ofstream logFile(logFilename, std::ios_base::app);
+        if (!logFile.is_open()) {
+            throw std::runtime_error("Cannot open log file for writing");
+        }
+        return logFile;
+    }
+
     // RAII color guard, that resets the console color after the scope ends.
     class Color {
     public:
-        Color(std::string_view color) { std::cout << "\033[" << color << "m"; }
-        ~Color() { std::cout << "\033[0m"; }
+        Color(std::string_view colorCode) {
+            std::cout << "\033[" << colorCode << "m";
+        }
+        ~Color() {
+            std::cout << "\033[0m";
+        }
     };
 
     /**
-     * Internal logging function.
-     * Don't use it directly. Use the macros instead.
+     * @internal
+     * You can use it, but there is probably a better way to do it, read README.md
      * @param level Log level of the message.
      * @param levelStr String label of the level.
      * @param color ANSI color code.
@@ -63,138 +205,160 @@ namespace mayak::logger {
      * @param line The line from which the log was called.
      */
     inline void _log(LogLevel level, std::string_view levelStr, std::string_view color, std::string_view msg, const char* file, int line) {
-        if (level < logLevel) return;
         std::lock_guard<std::mutex> lock(logMutex);
-        Color c(color);
-        std::cout << "[" << levelStr << "] " << msg;
+        std::string timestamp = getTimestamp(TimeFormat::DateTime);
+        if (colorLogging) {
+            Color colorGuard(color);
+            std::cout << "[" << timestamp << "] [" << levelStr << "] " << msg;
+        } else {
+            std::cout << "[" << timestamp << "] [" << levelStr << "] " << msg;
+        }
         if (additionalInfo)
             std::cout << " at " << file << ":" << line;
         std::cout << "\n";
     }
 
     /**
-     * Logs a trace message to the console.
-     * If debugging is enabled, it includes the file name and line number.
-     * @warning Is internal use only. Use MAYAK_LOG_TRACE() instead.
-     * 
-     * @param msg Trace message to log.
+     * @internal
+     * You can use it, but there is probably a better way to do it, read README.md
+     * @param level Log level of the message.
+     * @param levelStr String label of the level.
+     * @param msg The message to log.
+     * @param file The file from which the log was called.
+     * @param line The line from which the log was called.
      */
-    inline void _trace(std::string_view msg, const char* file, int line) {
-        _log(TRACE, "TRACE", "90", msg, file, line);
+    inline void _logToFile(LogLevel level, std::string_view levelStr, std::string_view msg, const char* file, int line) {
+        std::lock_guard<std::mutex> lock(logMutex);
+        getLogFile() << "[" << levelStr << "] " << msg;
+        if (additionalInfo)
+            getLogFile() << " at " << file << ":" << line;
+        getLogFile() << "\n";
     }
-
-    /**
-     * Logs a debug message to the console.
-     * If debugging is enabled, it includes the file name and line number.
-     * @warning Is internal use only. Use MAYAK_LOG_DEBUG() instead.
-     * 
-     * @param msg Debug message to log.
-     */
-    inline void _debug(std::string_view msg, const char* file, int line) {
-        _log(DEBUG, "DEBUG", "36", msg, file, line);
-    }
-
-    /**
-     * Logs an information message to the console.
-     * If debugging is enabled, it includes the file name and line number.
-     * @warning Is internal use only. Use MAYAK_LOG_INFO() instead.
-     * 
-     * @param msg Information message to log.
-     */
-    inline void _info(std::string_view msg, const char* file, int line) {
-        _log(INFO, "INFO", "37", msg, file, line);
-    }
-
-    /**
-     * Logs a warning message to the console.
-     * If debugging is enabled, it includes the file name and line number.
-     * @warning Is internal use only. Use MAYAK_LOG_WARN() instead.
-     * 
-     * @param msg Fatal error message to log.
-     */
-    inline void _warn(std::string_view msg, const char* file, int line) {
-        _log(WARN, "WARN", "33", msg, file, line);
-    }
-
-    /**
-     * Logs an error message to the console.
-     * If debugging is enabled, it includes the file name and line number.
-     * @warning Is internal use only. Use MAYAK_LOG_ERROR() instead.
-     * 
-     * @param msg Error message to log.
-     */
-    inline void _error(std::string_view msg, const char* file, int line) {
-        _log(ERROR, "ERROR", "31", msg, file, line);
-    }
-
-    /**
-     * Logs a fatal error message to the console.
-     * If debugging is enabled, it includes the file name and line number.
-     * @attention Should be used to exit the program.
-     * @warning Is internal use only. Use MAYAK_LOG_FATAL() instead.
-     * 
-     * @param msg Fatal error message to log.
-     */
-    inline void _fatal(std::string_view msg, const char* file, int line) {
-        _log(FATAL, "FATAL", "41;97", msg, file, line);
-    }
-}
+};
 
 // Public macros
 
 /**
- * @brief Logs a trace message to the console.
- * @note If debugging is enabled, it includes the file name and line number.
+ * @def MAYAK_LOG
+ * @brief Macro for logging messages.
+ * @param level The log level.
+ * @param colorCode ANSI color code.
+ * @param msg The message to log. Every std::string-compatible type is supported.
  * 
- * @param msg Trace message to log.
+ * Messages logged with this macro will be printed to the console if consoleLogging is true.
+ * Messages logged with this macro will be appended to the log file if fileLogging is true.
+ * 
+ * @see mayak::logger::setLogLevel
+ * @see mayak::logger::setAdditionalInfo
+ * @see mayak::logger::setFileLogging
+ * @see mayak::logger::setConsoleLogging
+ * @see mayak::logger::setColorLogging
  */
-#define MAYAK_LOG_TRACE(msg) mayak::logger::_trace(msg, __FILE__, __LINE__)
-
+#define MAYAK_LOG(level, colorCode, msg) \
+    do { \
+        if (mayak::logger::logLevel <= level){ \
+            if (mayak::logger::consoleLogging) mayak::logger::_log(level, #level, colorCode, msg, __FILE__, __LINE__); \
+            if (mayak::logger::fileLogging) mayak::logger::_logToFile(level, #level, msg, __FILE__, __LINE__); \
+        } \
+    } while (0)
 
 /**
- * @brief Logs a debug message to the console.
- * @note If debugging is enabled, it includes the file name and line number.
+ * @def MAYAK_LOG_TRACE
+ * @brief Macro for logging trace messages.
+ * @param msg The message to log. Every std::string-compatible type is supported.
  * 
- * @param msg Debug message to log.
+ * Messages logged with this macro will be printed to the console if consoleLogging is true.
+ * Messages logged with this macro will be appended to the log file if fileLogging is true.
+ * 
+ * @see mayak::logger::setLogLevel
+ * @see mayak::logger::setAdditionalInfo
+ * @see mayak::logger::setFileLogging
+ * @see mayak::logger::setConsoleLogging
+ * @see mayak::logger::setColorLogging
  */
-#define MAYAK_LOG_DEBUG(msg) mayak::logger::_debug(msg, __FILE__, __LINE__)
+#define MAYAK_LOG_TRACE(msg) MAYAK_LOG(mayak::logger::LogLevel::TRACE, "34", msg)
 
 /**
- * @brief Logs an information message to the console.
- * @note If debugging is enabled, it includes the file name and line number.
+ * @def MAYAK_LOG_DEBUG
+ * @brief Macro for logging debug messages.
+ * @param msg The message to log. Every std::string-compatible type is supported.
  * 
- * @param msg Information message to log.
+ * Messages logged with this macro will be printed to the console if consoleLogging is true.
+ * Messages logged with this macro will be appended to the log file if fileLogging is true.
+ * 
+ * @see mayak::logger::setLogLevel
+ * @see mayak::logger::setAdditionalInfo
+ * @see mayak::logger::setFileLogging
+ * @see mayak::logger::setConsoleLogging
+ * @see mayak::logger::setColorLogging
  */
-#define MAYAK_LOG_INFO(msg)  mayak::logger::_info(msg,  __FILE__, __LINE__)
+#define MAYAK_LOG_DEBUG(msg) MAYAK_LOG(mayak::logger::LogLevel::DEBUG, "36", msg)
 
 /**
- * @brief Logs a warning message to the console.
- * @note If debugging is enabled, it includes the file name and line number.
+ * @def MAYAK_LOG_INFO
+ * @brief Macro for logging info messages.
+ * @param msg The message to log. Every std::string-compatible type is supported.
  * 
- * @param msg Warning message to log.
+ * Messages logged with this macro will be printed to the console if consoleLogging is true.
+ * Messages logged with this macro will be appended to the log file if fileLogging is true.
+ * 
+ * @see mayak::logger::setLogLevel
+ * @see mayak::logger::setAdditionalInfo
+ * @see mayak::logger::setFileLogging
+ * @see mayak::logger::setConsoleLogging
+ * @see mayak::logger::setColorLogging
  */
-#define MAYAK_LOG_WARN(msg)  mayak::logger::_warn(msg,  __FILE__, __LINE__)
+#define MAYAK_LOG_INFO(msg) MAYAK_LOG(mayak::logger::LogLevel::INFO, "37", msg)
 
 /**
- * @brief Logs an error message to the console.
- * @note If debugging is enabled, it includes the file name and line number.
+ * @def MAYAK_LOG_WARN
+ * @brief Macro for logging warning messages.
+ * @param msg The message to log. Every std::string-compatible type is supported.
  * 
- * @param msg Error message to log.
+ * Messages logged with this macro will be printed to the console if consoleLogging is true.
+ * Messages logged with this macro will be appended to the log file if fileLogging is true.
+ * 
+ * @see mayak::logger::setLogLevel
+ * @see mayak::logger::setAdditionalInfo
+ * @see mayak::logger::setFileLogging
+ * @see mayak::logger::setConsoleLogging
+ * @see mayak::logger::setColorLogging
  */
-#define MAYAK_LOG_ERROR(msg) mayak::logger::_error(msg, __FILE__, __LINE__)
+    #define MAYAK_LOG_WARN(msg) MAYAK_LOG(mayak::logger::LogLevel::WARN, "33", msg)
 
 /**
- * @brief Logs a fatal error message to the console.
- * @note If debugging is enabled, it includes the file name and line number.
- * @attention Should be used to exit the program.
+ * @def MAYAK_LOG_ERROR
+ * @brief Macro for logging error messages.
+ * @param msg The message to log. Every std::string-compatible type is supported.
  * 
- * @param msg Fatal error message to log.
+ * Messages logged with this macro will be printed to the console if consoleLogging is true.
+ * Messages logged with this macro will be appended to the log file if fileLogging is true.
+ * 
+ * @see mayak::logger::setLogLevel
+ * @see mayak::logger::setAdditionalInfo
+ * @see mayak::logger::setFileLogging
+ * @see mayak::logger::setConsoleLogging
+ * @see mayak::logger::setColorLogging
  */
-#define MAYAK_LOG_FATAL(msg) mayak::logger::_fatal(msg, __FILE__, __LINE__)
+    #define MAYAK_LOG_ERROR(msg) MAYAK_LOG(mayak::logger::LogLevel::ERR, "31", msg)
 
-// TODO: Add color disable if no color support
-// TODO: Add logging to file
+/**
+ * @def MAYAK_LOG_FATAL
+ * @brief Macro for logging fatal messages.
+ * @param msg The message to log. Every std::string-compatible type is supported.
+ * 
+ * Messages logged with this macro will be printed to the console if consoleLogging is true.
+ * Messages logged with this macro will be appended to the log file if fileLogging is true.
+ * 
+ * @see mayak::logger::setLogLevel
+ * @see mayak::logger::setAdditionalInfo
+ * @see mayak::logger::setFileLogging
+ * @see mayak::logger::setConsoleLogging
+ * @see mayak::logger::setColorLogging
+ */
+#define MAYAK_LOG_FATAL(msg) MAYAK_LOG(mayak::logger::LogLevel::FATAL, "41;97", msg)
+
 // TODO: Add operator <<
 // TODO: Add better formatting
-// TODO: Add timestamp
 // TODO: Use std::clog / std::cerr instead of std::cout
+// TODO: Add custom exceptions
